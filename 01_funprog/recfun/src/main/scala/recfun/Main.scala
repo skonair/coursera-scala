4527772830049ad008d11c540a34a832b92efed0
Sascha Koch <sascha.koch@web.de>
2016-05-24 23:37:41 +0200

    perenthesis balance implemented

--- 01_funprog/recfun/src/main/scala/recfun/Main.scala
+++ 01_funprog/recfun/src/main/scala/recfun/Main.scala
@@ -13,7 +13,7 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = (c, r) match {
+  def pascal(c: Int, r: Int): Int = (c, r) match {
     case (_, 0) => 1
     case (0, _) => 1
     case (_, _) => if (c == r) 1 else pascal(c - 1, r - 1) + pascal(c, r - 1)
@@ -22,7 +22,14 @@ object Main {
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+  def balanceAcc(chars: List[Char], count: Int): Boolean = chars match {
+    case ('(' :: as) => balanceAcc(as, count + 1)
+    case (')' :: as) => if (count == 0) false else balanceAcc(as, count - 1)
+    case (_ :: as)  => balanceAcc(as, count)
+    case Nil => count == 0
+  }
+
+    def balance(chars: List[Char]): Boolean = balanceAcc(chars, 0)
   
   /**
    * Exercise 3
