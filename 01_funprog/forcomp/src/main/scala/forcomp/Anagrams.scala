4b835c93bd6a659cec5528e3300cf8341373dd70
Sascha Koch <sascha.koch@web.de>
2016-06-18 21:07:36 +0200

    type fixes

--- 01_funprog/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ 01_funprog/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -67,7 +67,7 @@ object Anagrams {
     .mapValues(e => e.map(f => f._2))
 
   /** Returns all the anagrams of a given word. */
-  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences}}.getOrElse(wordOccurrences(word), List.empty)
+  def wordAnagrams(word: Word): List[Word] = dictionaryByOccurrences.getOrElse(wordOccurrences(word), List.empty)
 
   /** Returns the list of all subsets of the occurrence list.
    *  This includes the occurrence itself, i.e. `List(('k', 1), ('o', 1))`
